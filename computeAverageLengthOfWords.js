360c99e9344829f61ad2b830d7929d687be9befc
AlexSfc04 <amoricoalejandro@alumnos.cei.es>
2026-01-14 11:09:25 +0100

    ejerecicios

---
+++ computeAverageLengthOfWords.js
@@ -0,0 +1,13 @@
+// EJERCICIO 1. computeAverageLengthOfWords
+/* Escribe una función 'computeAverageLengthOfWords' que recibe dos strings y devuelve el promedio de la longitud de ambos strings.
+
+
+Ejemplo de entrada:
+*/
+const output = computeAverageLengthOfWords('code', 'programs');
+console.log(output); // --> 6 
+
+function computeAverageLengthOfWords(word1, word2) {
+  // your code here
+  return (word1.length + word2.length) / 2;
+}
